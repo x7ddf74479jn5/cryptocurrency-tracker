92c6f56c050b421c5c5caba251e8cf88520154f5
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-05-05 02:21:55 +0900

    🐛 fix: swr cache

--- src/usecases/coin.ts
+++ src/usecases/coin.ts
@@ -12,19 +12,19 @@ import { fetchCoinList, fetchHistoricalChart, fetchSingleCoin, fetchTrendingCoin
 const coinCacheKeyGenerator = createCacheKeyGenerator("coin");
 
 export const useCoinList = (currency: string) => {
-  return useSWR<CoinListResponse>(coinCacheKeyGenerator("list"), () => fetchCoinList(currency));
+  return useSWR<CoinListResponse>(coinCacheKeyGenerator("list", currency), () => fetchCoinList(currency));
 };
 
 export const useHistoricalChart = (id: string, currency: string, days?: number) => {
-  return useSWR<HistoricalChartResponse>(coinCacheKeyGenerator("chart"), () =>
+  return useSWR<HistoricalChartResponse>(coinCacheKeyGenerator("chart", id, currency, days?.toString() ?? ""), () =>
     fetchHistoricalChart(id, days, currency)
   );
 };
 
 export const useSingleCoin = (id: string) => {
-  return useSWR<SingleCoinResponse>(coinCacheKeyGenerator("single"), () => fetchSingleCoin(id));
+  return useSWR<SingleCoinResponse>(coinCacheKeyGenerator("single", id), () => fetchSingleCoin(id), { suspense: true });
 };
 
 export const useTrendingCoins = (currency: string) => {
-  return useSWR<TrendingCoinsResponse>(coinCacheKeyGenerator("trending"), () => fetchTrendingCoins(currency));
+  return useSWR<TrendingCoinsResponse>(coinCacheKeyGenerator("trending", currency), () => fetchTrendingCoins(currency));
 };
