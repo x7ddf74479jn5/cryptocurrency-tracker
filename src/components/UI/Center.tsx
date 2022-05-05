83890f390a485c4b0721dde73959194efd8752ce
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-05-06 00:07:50 +0900

    ♻️ refactor: Center

--- src/components/UI/Center.tsx
+++ src/components/UI/Center.tsx
@@ -3,6 +3,7 @@ import { styled } from "@mui/system";
 export const Center = styled("div")({
   textAlign: "center",
   display: "flex",
+  width: "100%",
   justifyContent: "center",
   alignItems: "center",
 });
